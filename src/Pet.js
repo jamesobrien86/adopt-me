901011a2a3beef1167f2d1b712c441a07ddc0866
jamesobrien86 <jamesobrien.web@gmail.com>
2020-06-18 14:00:11 +0100

    impl - eslint config for plugins

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,11 @@
 import React from "react";
+
 export default function Pet({ name, type, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, type),
-    React.createElement("h3", {}, breed),
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{type}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
