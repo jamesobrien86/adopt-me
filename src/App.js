a2a65f90c2b23cddd5ca4d485596c73809ecd264
jamesobrien86 <jamesobrien.web@gmail.com>
2020-07-08 09:54:53 +0100

    impl - updated babel and lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
-import ThemeContext from "./ThemeContext";
-import NavBar from "./NavBar";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Link } from '@reach/router';
+import Details from './Details';
+import SearchParams from './SearchParams';
+import ThemeContext from './ThemeContext';
+import NavBar from './NavBar';
 
 const App = () => {
-  const theme = useState("darkblue");
+  const theme = useState('darkblue');
   return (
     <ThemeContext.Provider value={theme}>
       <div>
@@ -21,4 +21,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
